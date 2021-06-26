eae797de2d0ed4743b8ad7a1ac8b04d0ce2ab14f
GitHub Action <action@github.com>
2021-06-26 21:12:31 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kloostermanw/phpstormceleryplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kloostermanw.phpstormceleryplugin.services
+
+import com.github.kloostermanw.phpstormceleryplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
